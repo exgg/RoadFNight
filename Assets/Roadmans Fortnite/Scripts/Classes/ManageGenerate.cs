34b6d8f66a92d7ca2ea0560315a2cdbbecda930d
Samuel Clutton <samuelcluttonwork@gmail.com>
2024-08-05 14:57:33 +0100

    Changed prefab layers

--- Assets/Roadmans Fortnite/Scripts/Classes/ManageGenerate.cs
+++ Assets/Roadmans Fortnite/Scripts/Classes/ManageGenerate.cs
@@ -2,20 +2,43 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Serialization;
 
 public class ManageGenerate : MonoBehaviour
 {
     public CityGen generateCity;
+    public NavMeshSurface aISurface;
 
-    public void GenerateCity()
+    private void Start()
+    {
+        if (!generateCity)
+            generateCity = FindObjectOfType<CityGen>();
+        
+        if (!aISurface)
+            aISurface = FindObjectOfType<NavMeshSurface>();
+
+        if (generateCity && aISurface)
+        {
+            GenerateCity();
+            GenerateNavMesh();
+        }
+    }
+
+    private void GenerateCity()
     {
         print("Generating City");
-        
+
         generateCity.ClearBuildings();
         generateCity.ClearBlocks();
         generateCity.ClearPerimeter();
         generateCity.Generate();
         generateCity.GenerateBuildings();
+    }
+
+    private void GenerateNavMesh()
+    {
+        print("building navmesh");
+        aISurface.BuildNavMesh();
     }
 }
