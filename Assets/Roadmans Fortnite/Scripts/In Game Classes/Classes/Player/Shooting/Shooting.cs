f96a4abc1965af5c7008e21ed48b72a9a088ce98
Samuel Clutton <samuelcluttonwork@gmail.com>
2024-08-22 14:50:17 +0100

    Revert "Merge branch 'Shooting'"
    This reverts commit 61f57578894f57754913a48dc4067eaddc489313, reversing
    changes made to 7a4d9aeb7739c45dea58db9210220bfc7f2ca435.

--- Assets/Roadmans Fortnite/Scripts/In Game Classes/Classes/Player/Shooting/Shooting.cs
+++ Assets/Roadmans Fortnite/Scripts/In Game Classes/Classes/Player/Shooting/Shooting.cs
@@ -1,46 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using Mirror;
 
-namespace Roadmans_Fortnite.Scripts.Classes.Player.Shooting
+public class Shooting : MonoBehaviour
 {
-    public class Shooting : NetworkBehaviour
+    // Start is called before the first frame update
+    void Start()
     {
+        
+    }
 
-        public GameObject bulletPrefab;
-        public GameObject rocketPrefab;
-        public float bulletSpeed;
-        Transform _bulletSpawnPointPosition;
-
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
-        public void AimWeapon()
-        {
-            // if (player NOT in any menu && NOT emoting && NOT dead && has weapon equiped) {CmdAim()}
-        }
-
-        public void ShootWeapon()
-        {
-
-        }
-
-        [Command]
-        public void CmdAim()
-        {
-            // TPControllerManager.aimValue = 1;
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
