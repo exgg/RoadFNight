89ed3d2893ffbbcf4e6584662987afb813594409
Jianfeng <981393625@qq.com>
2024-09-16 16:46:36 +0100

    fixed(Add update predominant race function to district)

--- Assets/MAD City/Scripts/District.cs
+++ Assets/MAD City/Scripts/District.cs
@@ -9,14 +9,27 @@ public class District : MonoBehaviour
     public DistrictData district_data;
 
     public Race predominant_race;//>50%
+    public int predominant_race_num;
     public Dictionary<Race, float> race_lst = new Dictionary<Race, float>();//race, ratio
 
+    // TODO: Need to change the type from GameObject to Civilians 
+    public List<GameObject> black_people_lst = new List<GameObject>();
+    public List<GameObject> white_people_lst = new List<GameObject>();
+    public List<GameObject> asian_people_lst = new List<GameObject>();
+    public List<GameObject> halfcast_people_lst = new List<GameObject>();
+    public List<List<GameObject>> all_people_lst = new List<List<GameObject>>();
     public void init_district(List<DistrictData> _districtData, int district_num)
     {
         district_data = _districtData[level];
         predominant_race = (Race)Random.Range(0, (int)Race.RaceNum);
         name = "District - No:" + district_num;
         generate_race_ratio();
+        if_change_predominant_race();//set predominant_race_num
+
+        all_people_lst.Add(black_people_lst);
+        all_people_lst.Add(white_people_lst);
+        all_people_lst.Add(asian_people_lst);
+        all_people_lst.Add(halfcast_people_lst);
     }
 
     //The generated proportions all conform to 0.0 format
@@ -46,6 +59,45 @@ public class District : MonoBehaviour
             }
             //Debug.LogFormat("race_lst[{0}] = {1}", i, race_lst[i]);
             cnt += 1;
+        }
+    }
+
+    public bool if_change_predominant_race()
+    {
+        switch (predominant_race)
+        {
+            case Race.Black:
+                predominant_race_num = black_people_lst.Count;
+                return black_people_lst.Count < district_data.volume / 2 ? true : false;
+            case Race.White:
+                predominant_race_num = white_people_lst.Count;
+                return white_people_lst.Count < district_data.volume / 2 ? true : false;
+            case Race.Asian:
+                predominant_race_num = asian_people_lst.Count;
+                return asian_people_lst.Count < district_data.volume / 2 ? true : false;
+            case Race.HalfCast:
+                predominant_race_num = halfcast_people_lst.Count;
+                return halfcast_people_lst.Count < district_data.volume / 2 ? true : false;
+            default:
+                return false;
+        }
+    }
+
+    //When respawn new npc, predominant race might change
+    public void update_predominant_race()
+    {
+        if (if_change_predominant_race())
+        {
+            int tmp = 0;
+            foreach (var i in all_people_lst)
+            {
+                if (i.Count > tmp)
+                {
+                    tmp = i.Count;
+                    //TODO: change the race
+                    //predominant_race = i[0].race;
+                }
+            }
         }
     }
 }
