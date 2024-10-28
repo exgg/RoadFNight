dab36e5efc8f08f2cd8cc1da61a474effb47b4a1
Samuel Clutton <samuelcluttonwork@gmail.com>
2024-10-28 16:49:53 +0000

    dialogue is now predominantly implemented and just awaiting a filled out csv

--- Assets/Roadmans Fortnite/Scripts/In Game Classes/Classes/AI/PrejudiceEngine/PedestrianDialogueManager.cs
+++ Assets/Roadmans Fortnite/Scripts/In Game Classes/Classes/AI/PrejudiceEngine/PedestrianDialogueManager.cs
@@ -1,7 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
 using Roadmans_Fortnite.Scripts.Data_Reading;
+using Roadmans_Fortnite.Scripts.In_Game_Classes.Classes.AI.Base;
 using Roadmans_Fortnite.Scripts.In_Game_Classes.Classes.AI.Scriptable_Objects;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Roadmans_Fortnite.Scripts.In_Game_Classes.Classes.AI.PrejudiceEngine
 {
@@ -9,19 +13,74 @@ namespace Roadmans_Fortnite.Scripts.In_Game_Classes.Classes.AI.PrejudiceEngine
     {
         [SerializeField] private TextMeshProUGUI dialogue;
         private CsvReaderDialogue _csvReaderDialogue;
+
+        [SerializeField] private float dialogueTimer;
+        [SerializeField] private float dialogueCooldown;
+        
+        private bool _startedDialogue;
+        private bool _dialogueOnCooldown;
+        
+        private StateHandler _stateHandler;
+        private Pedestrian _pedestrian;
         
         void Start()
         {
-            _csvReaderDialogue = FindObjectOfType<CsvReaderDialogue>();
+            dialogue.transform.parent.gameObject.SetActive(false);
             
-            GetRandomDialogueFromLookup();
+            _csvReaderDialogue = FindObjectOfType<CsvReaderDialogue>();
+            _stateHandler = GetComponent<StateHandler>();
+            _pedestrian = GetComponent<Pedestrian>();
         }
 
-        private void GetRandomDialogueFromLookup()
+        public void StartDialogue(string speaker, string target, string category)
         {
-            string test = _csvReaderDialogue.GetRandomDialogue("Pagan", "Christian","Neutral");
-
-            dialogue.text = test;
+            Debug.Log("Starting dialogue");
+            
+            if (_startedDialogue && !_dialogueOnCooldown)
+                return;
+            
+            
+            
+            StartCoroutine(TriggerDialogue(speaker, target, category));
         }
+        
+        private IEnumerator TriggerDialogue(string speaker, string target, string category)
+        {
+            Debug.Log("Looking for dialogue");
+
+            _startedDialogue = true;
+            
+            GetRandomDialogueFromLookup(speaker, target, category);
+            dialogue.transform.parent.gameObject.SetActive(true);
+            
+            yield return new WaitForSeconds(dialogueTimer);
+            
+            dialogue.transform.parent.gameObject.SetActive(false);
+            _startedDialogue = false;
+            _dialogueOnCooldown = true;
+            
+            
+            if (_stateHandler.InteractionChecker(_pedestrian.aggressionLevel, _pedestrian.confidenceLevel) !=
+                "Aggressive")
+                _stateHandler.currentTarget = null;
+
+            StartCoroutine(StartCooldown());
+        }
+
+        private IEnumerator StartCooldown()
+        {
+            yield return new WaitForSeconds(dialogueCooldown);
+
+            _dialogueOnCooldown = false;
+        }
+        
+        private void GetRandomDialogueFromLookup(string speaker, string target, string category)
+        {
+            string text = _csvReaderDialogue.GetRandomDialogue(speaker, target, category);
+
+            dialogue.text = text;
+        }
+
+   
     }
 }
